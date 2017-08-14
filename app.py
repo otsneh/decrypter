8bebe0fc6533cf1f622a2770e47941006469ffcf
Ophir Sneh <otsneh@gmail.com>
2017-08-14 02:45:41 -0700

    switch to render_template from serve_static

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
-app = Flask(__name__,  static_folder='public')
+app = Flask(__name__,  template_folder='public')
 
 @app.route('/')
 def home():
-    return app.send_static_file('index.html')
-
-@app.route('/dist/<path:filename>')
-def serve_dist(filename):
-    return app.send_from_directory('/dist/', filename)
+    return render_template('index.html')
